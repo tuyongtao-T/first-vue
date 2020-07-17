74f6797c8ad6c05d11b45ca7951349188d2e2df9
tuyongtao-T <49391988+tuyongtao-T@users.noreply.github.com>
2020-07-17 21:29:51 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template >
+  <div>
+    <First v-for="(item,index) in persons" :age="item.age" :name="item.name" :key="index"/>
+    <input type="text" v-model="newPerson.name">
+    <input type="text" v-model="newPerson.age">
+    <button @click="addPerson"> 添加</button>
+    <button @click="delPerson">删除</button>
+  </div>
+</template>
+
+<script>
+import First from "./components/First"
+export default {
+     data: function (){
+       return {
+         persons: [
+            {name:'小花',age:'21'},
+            {name:'小狼',age:'23'},
+            {name:'小红',age:'19'},
+         ],
+         newPerson:{
+           name: '',
+           age:0
+         }
+      }
+    },
+    methods: {
+      addPerson(){
+          this.persons.push(this.newPerson)
+      },
+      delPerson(){
+        this.persons.pop()
+      }
+    },
+    components: {
+      First
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/First.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="First">
+        <h6>你好，现在是星期{{new Date().getDay()}}</h6>
+        <p>我是{{name}},今年{{age}}岁</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        age:{
+            required: true
+        },
+        name:{
+            type: String,
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>>
+
